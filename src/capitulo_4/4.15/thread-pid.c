0f8a684b5683477443d2082d0c8086ad8a25b313
rbvaz <romi.alfonzo99@fpuna.com.py>
2021-09-13 22:07:12 -0400

    Listing 4.13 terminado

--- src/capitulo_4/4.15/thread-pid.c
+++ src/capitulo_4/4.15/thread-pid.c
@@ -4,19 +4,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void* thread_function (void* arg)
+void *thread_function(void *arg)
 {
-fprintf (stderr, “child thread pid is %d\n”, (int) getpid ());
-/* Spin forever. */
-while (1);
-return NULL;
+    fprintf(stderr, "child thread pid is % d\n", (int)getpid());
+    /* Spin forever. */
+    while (1)
+        ;
+    return NULL;
 }
-int main ()
+int main()
 {
-pthread_t thread;
-fprintf (stderr, “main thread pid is %d\n”, (int) getpid ());
-pthread_create (&thread, NULL, &thread_function, NULL);
-/* Spin forever. */
-while (1);
-return 0;
+    pthread_t thread;
+    fprintf(stderr, "main thread pid is % d\n", (int)getpid());
+    pthread_create(&thread, NULL, &thread_function, NULL);
+    /* Spin forever. */
+    while (1)
+        ;
+    return 0;
 }
